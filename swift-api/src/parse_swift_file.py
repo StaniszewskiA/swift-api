cda46f453068dab5bd81c12ca67ff4116b7d87ba
AStaniszewski <staniszewski01adam@gmail.com>
2025-04-26 14:36:25 +0200

    [Parser] Clean up .xlsx parser code

--- swift-api/src/parse_swift_file.py
+++ swift-api/src/parse_swift_file.py
@@ -2,23 +2,32 @@ import pandas as pd
 
 
 def parse_swift_file(file_path: str) -> None:
+    """
+    Parses a SWIFT .xlsx file and saves the cleaned data to a database table.
+
+    Parameters
+    ----------
+        file_path : str
+            Path to the .xlsx file
+
+    Returns
+    -------
+    None
+    """
     try:
         df = pd.read_excel(file_path)
 
-        if "COUNTRY ISO2 CODE" in df.columns:
-            df["COUNTRY ISO2 CODE"] = df["COUNTRY ISO2 CODE"].str.upper()
-        if "COUNTRY NAME" in df.columns:
-            df["COUNTRY NAME"] = df["COUNTRY NAME"].str.upper()
+        for col in ("COUNTRY ISO2 CODE", "COUNTRY NAME"):
+            if col in df.columns:
+                df[col] = df[col].str.upper()
 
-        df["Is Headquarters"] = df["SWIFT CODE"].str.endswith("XXX")
-        df["Headquarters CODE"] = df.apply(
-            lambda row: row["SWIFT CODE"] if row["Is Headquarters"] else row["SWIFT CODE"][:8] + "XXX", axis=1
-        )
+        if "SWIFT CODE" in df.columns:
+            df["Is Headquarters"] = df["SWIFT CODE"].str.endswith("XXX")
+            df["Headquarters CODE"] = df.apply(
+                lambda row: row["SWIFT CODE"] if row["Is Headquarters"] else row["SWIFT CODE"][:8] + "XXX", axis=1
+            )
 
-        """
-            I think that time zone may be omitted - we can figure it out based on 
-            TOWN NAME and COUNTRY NAME columns.
-        """
+        # Time zone is redundant is we know the city.
         redundant_cols = ["TIME ZONE"]
         df = df.drop(columns=[col for col in redundant_cols if col in df.columns])
 
